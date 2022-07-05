3094565294c32eeed9619cf217662bd523cc1351
Kodama <fk@fkodama.com>
2022-07-05 17:15:40 -0400

    Create basic pages

---
+++ frontend/pages/api/affirmation.ts
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const affirmation = () => {
+  return <div></div>;
+};
+
+export default affirmation;
---
+++ frontend/pages/shortcut.ts
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const shortcut = () => {
+  return <div></div>;
+};
+
+export default shortcut;
